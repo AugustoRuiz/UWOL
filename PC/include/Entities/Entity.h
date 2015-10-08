cd9af90e7167d1c8e811a335ab5644307ffe766a
Augusto Ruiz <augusto.ruiz@gmail.com>
2015-10-08 20:46:16 +0200

    Removed Typo

---
+++ PC/include/Entities/Entity.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <map>
+#include <vector>
+#include <string>
+#include "Component.h"
+#include "Log.h"
+
+using namespace std;
+
+class Component;
+
+typedef pair<string, Component*> COMPONENT_ENTRY;
+typedef map<string, Component*> COMPONENT_MAP;
+
+typedef vector<Component*> COMPONENTS;
+typedef map<string, COMPONENTS> MESSAGE_REGISTER_MAP;
+typedef pair<string, COMPONENTS> MESSAGE_REGISTER_ENTRY;
+
+class Entity {
+	COMPONENT_MAP components;
+	MESSAGE_REGISTER_MAP messageRegistration;
+
+public:
+	void AddComponent(string name, Component *value);
+	Component* RemoveComponent(string name);
+	Component* GetComponent(string name);
+	void SendMessage(Component *source, string message, void* data);
+
+	void Update(int milliSecs);
+};
