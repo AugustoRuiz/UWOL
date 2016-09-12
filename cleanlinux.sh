9d0d7d519b37ef2744b3603fac27d03c934df23a
Augusto Ruiz <augusto.ruiz@gmail.com>
2016-09-12 23:56:32 +0200

    Added linux shell build helpers

---
+++ cleanlinux.sh
@@ -0,0 +1,8 @@
+cd RW.PackLib
+make -f Makefile.linux clean
+cd ..
+cd RW.GameLib
+make -f Makefile.linux clean
+cd ..
+cd PC
+make -f Makefile.linux clean
---
+++ makelinux.sh
@@ -0,0 +1,8 @@
+cd RW.PackLib
+make -f Makefile.linux
+cd ..
+cd RW.GameLib
+make -f Makefile.linux
+cd ..
+cd PC
+make -f Makefile.linux
